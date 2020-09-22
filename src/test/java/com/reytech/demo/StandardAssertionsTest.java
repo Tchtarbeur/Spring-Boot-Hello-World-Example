c594b8398cdf8d4d11c39258057b1a0afccdf02b
Tchtarbeur <58663078+Tchtarbeur@users.noreply.github.com>
2020-09-22 11:46:26 +0200

    Update StandardAssertionsTest.java

--- src/test/java/com/reytech/demo/StandardAssertionsTest.java
+++ src/test/java/com/reytech/demo/StandardAssertionsTest.java
@@ -32,7 +32,7 @@ class StandardAssertionsTest {
     void groupedAssertions() {
         Personne pers = new Personne("Nassur", "Moumadi");
 
-        assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
+        assertAll("personne", () -> assertEquals("Nass", pers.getFirstName()),
                               () -> assertEquals("Moumadi", pers.getLastName()));
     }
 
